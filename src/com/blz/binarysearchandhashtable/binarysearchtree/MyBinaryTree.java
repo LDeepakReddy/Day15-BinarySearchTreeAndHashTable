065740eb952624cc9431f54776eb283d69684c82
LDeepakReddy <l.deepakreddy.com>
2022-04-17 04:52:54 +0530

    Added:Welcome

---
+++ src/com/blz/binarysearchandhashtable/binarysearchtree/MyBinaryTree.java
@@ -0,0 +1,7 @@
+package com.blz.binarysearchandhashtable.binarysearchtree;
+
+public class MyBinaryTree {
+    public static void main(String[] args){
+        System.out.println("Welcome");
+    }
+}
